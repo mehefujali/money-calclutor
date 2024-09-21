a69a40a8eb77d37f526ab353cee50dceeb75399d
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-21 19:59:25 +0530

    seving problem selve

--- script.js
+++ script.js
@@ -6,15 +6,27 @@ document.getElementById('calculate-btn').addEventListener('click', function (eve
       const courses = getInputValueById('courses')
       const internet = getInputValueById('internet')
       const totalCost = software + courses + internet
-      const balance = income - totalCost
+      const newBalance = income - totalCost
       const resultDisplay = document.getElementById('results')
       resultDisplay.classList.remove('hidden')
-      document.getElementById('total-expenses').innerText = totalCost
+      const toalExpenses = document.getElementById('total-expenses')
+      toalExpenses.innerText = parseFloat(toalExpenses.innerText) totalCost
+      const balance = parseFloat(document.getElementById('balance').innerText) + newBalance
       document.getElementById('balance').innerText = balance
+      return (balance)
 })
 document.getElementById('calculate-savings').addEventListener('click', function () {
-
+      const income = getInputValueById('income')
+      const software = getInputValueById('software')
+      const courses = getInputValueById('courses')
+      const internet = getInputValueById('internet')
+      const totalCost = software + courses + internet
+      const newBalance = income - totalCost
+      const remainingBalance = document.getElementById('remaining-balance')
       const savings = getInputValueById('savings')
-      const parentage = (savings / 100) * balance
-      const avableBalench = balance - parentage
+      const sevingAmout = document.getElementById('savings-amount')
+      const parentage = (savings / 100) * newBalance
+      const avableBalench = newBalance - parentage
+      remainingBalance.innerText = parseFloat(remainingBalance.innerText) + avableBalench
+      sevingAmout.innerText = parseFloat(sevingAmout.innerText) + parentage
 })
